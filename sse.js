5e8d2d36903da406296f1f0824059e3e9920c0c0
jeff <jebberwocky@gmail.com>
2024-03-05 17:50:00 +0800

    remove typo

--- sse.js
+++ sse.js
@@ -41,7 +41,6 @@ app.get("/status", (req, res) =>{
 
 app.post("/beta/v4", (req, res) => {
   const userInput = req.body.input;
-
   //set streaming headers
   res.set({
     "Access-Control-Allow-Origin": "*",
@@ -54,22 +53,20 @@ app.post("/beta/v4", (req, res) => {
     model: 'gpt-4',
     messages: [{ 
       role: 'user', 
-      content:userInput}],// '解卦: ["888679", "觀", "萃", {"0": "盥而不荐，有孚顒若。", "1": "初六：童觀，小人無咎，君子吝。", "2": "六二：窺觀，利女貞。", "3": "六三：觀我生，進退。", "4": "六四：觀國之光，利用賓於王。", "5": "九五：觀我生，君子無咎。", "6": "上九：觀其生，君子無咎。", "7": "彖︰大觀在上，順而巽，中正以觀天下。觀，盥而不薦，有孚顒若，下觀而化也。觀天之神道，而四時不忒，聖人以神道設教，而天下服矣。"}, ["【觀之萃】", "動爻有【2】根。", "主要看【上九】，其次看【六四】。", "上九：觀其生，君子無咎。", "六四：觀國之光，利用賓於王。"]]' }],
+      content:userInput}],
     stream: true,
   });
   stream.on('content', (delta, snapshot) => {
     console.log("delta:"+delta)
     res.write(delta);
   });
-stream.on('finalChatCompletion', (completion) => {
-	console.log("it ends")
-	console.log(JSON.stringify(completion))
-	res.end();
-})
-  //
+  stream.on('finalChatCompletion', (completion) => {
+    console.log("finalChatCompletion")
+    console.log(JSON.stringify(completion))
+    res.end();
+  })
   res.on("close", () => {
-    //clearInterval(interval);
-stream.abort()
+    stream.abort()
     console.log("close")
     res.end();
   });
